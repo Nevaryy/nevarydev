43e3cd43f0e1fe6983d548de4a17701690823960
Marc W <marc.werthschulte@gmail.com>
2024-02-16 16:21:05 +0100

    add pages to page module

--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PageRoutingModule } from './page-routing.module';
 import { ComponentModule } from '../components/component.module';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [HomeComponent, AboutMeComponent, ImprintComponent],
     imports: [CommonModule, PageRoutingModule, ComponentModule],
+    providers: [],
 })
 export class PageModule {}
