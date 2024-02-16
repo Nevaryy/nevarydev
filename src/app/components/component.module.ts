f88d153e88f80220f179b9d7e1e73104e13bc1db
Marc W <marc.werthschulte@gmail.com>
2024-02-16 17:40:29 +0100

    add i18n to sub modules

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [NavigationComponent],
-    imports: [CommonModule, MaterialModule, RouterModule],
+    imports: [CommonModule, MaterialModule, RouterModule, TranslateModule],
     exports: [NavigationComponent],
 })
 export class ComponentModule {}
--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -6,10 +6,18 @@ import { ComponentModule } from '../components/component.module';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
     declarations: [HomeComponent, AboutMeComponent, ImprintComponent],
-    imports: [CommonModule, PageRoutingModule, ComponentModule],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        PageRoutingModule,
+        ComponentModule,
+        TranslateModule,
+    ],
     providers: [],
 })
 export class PageModule {}
