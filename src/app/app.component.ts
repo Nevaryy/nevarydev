1fb94cfb520a6cea4d0451f1321d12bd2f787eda
Marc W <marc.werthschulte@gmail.com>
2024-02-16 11:22:48 +0100

    change app prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'nevarydev-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    imports: [NxWelcomeComponent, RouterModule],
+    selector: 'nev-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'nevarydev';
+    title = 'nevarydev';
 }
