037231be39ef77a259e3647ca8aa05bb3fdf8114
Marc W <marc.werthschulte@gmail.com>
2024-02-16 12:39:12 +0100

    add base styling setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+    declarations: [AppComponent, NavigationComponent],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes), MaterialModule],
     providers: [provideClientHydration(), provideAnimationsAsync()],
     bootstrap: [AppComponent],
+    exports: [NavigationComponent],
 })
 export class AppModule {}
