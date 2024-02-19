ff67f49e738eb7e797c3576a77d136a941d2999d
Marc W <marc.werthschulte@gmail.com>
2024-02-19 16:41:55 +0100

    Add wait for theme load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslationService } from './services/translation.service';
+import { Store } from '@ngrx/store';
+import { selectThemeLoaded } from './store/theme-state/theme.selectors';
 
 @Component({
     selector: 'nev-root',
@@ -7,7 +9,12 @@ import { TranslationService } from './services/translation.service';
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    constructor(private translationService: TranslationService) {
+    themeLoaded$ = this.store.select(selectThemeLoaded);
+
+    constructor(
+        private translationService: TranslationService,
+        private store: Store
+    ) {
         this.translationService.initialize();
     }
 }
