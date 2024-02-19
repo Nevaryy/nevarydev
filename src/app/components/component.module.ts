58cfb0235c1d73d528b4c2b4dbda00a54b3b4d5d
Marc W <marc.werthschulte@gmail.com>
2024-02-19 12:34:48 +0100

    Add SidenavComponent to ComponentModule

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
-import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent],
-    imports: [CommonModule, MaterialModule, RouterModule, TranslateModule],
-    exports: [HeaderComponent, FooterComponent],
+    declarations: [HeaderComponent, FooterComponent, SidenavComponent],
+    imports: [CommonModule, MaterialModule, TranslateModule],
+    exports: [
+        MaterialModule,
+        TranslateModule,
+        HeaderComponent,
+        FooterComponent,
+        SidenavComponent,
+    ],
 })
 export class ComponentModule {}
