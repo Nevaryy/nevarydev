905e3df68aa0a236e6b165817b0b6cac6c02d9a8
Marc W <marc.werthschulte@gmail.com>
2024-02-21 11:24:56 +0100

    add env files

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    websiteRepoUrl: 'https://github.com/Nevaryy/nevarydev',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    websiteRepoUrl: 'https://github.com/Nevaryy/nevarydev',
+};
